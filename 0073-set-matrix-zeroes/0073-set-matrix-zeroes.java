86a600cdbe09c408f7a33010021cc45935a440a1
GONTHINA SIVA BHASKAR <gonthinasivabhaskar6@gmail.com>
2026-06-11 22:18:46 +0530

    Time: 1 ms (98.42%), Space: 47.4 MB (90.26%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,28 +1,34 @@
 class Solution {
-    public void setZeroes(int[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
+    public void setZeroes(int[][] matrix) 
+    {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        boolean row[] = new boolean[n];
+        boolean col[] = new boolean[m];
 
-        boolean[] zeroRows = new boolean[rows];
-        boolean[] zeroCols = new boolean[cols];
-
-        // First pass: mark rows and cols that need to be zeroed
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0) {
-                    zeroRows[i] = true;
-                    zeroCols[j] = true;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    row[i]=true;
+                    col[j]=true;
                 }
             }
         }
-
-        // Second pass: update matrix
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (zeroRows[i] || zeroCols[j]) {
-                    matrix[i][j] = 0;
-                }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+               if(row[i]==true||col[j]==true)
+               {
+                matrix[i][j]=0;
+               }
             }
         }
+
+
+        
     }
 }
