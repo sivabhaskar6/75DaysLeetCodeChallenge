6499766764f005be37fa1d034fcafdf7a5d3333a
GONTHINA SIVA BHASKAR <gonthinasivabhaskar6@gmail.com>
2026-07-08 13:48:06 +0530

    Time: 0 ms (100%), Space: 45.7 MB (96.61%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.java
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -1,14 +1,7 @@
 class Solution {
     public double[] convertTemperature(double celsius) 
     {
-        double[] arr = new double[2];
-
-        double kel = celsius + 273.15;
-        double far =celsius * 1.80 + 32.00;
-
-        arr[0]=kel;
-        arr[1]=far;
-
-        return arr;
+        
+        return new double[]{ celsius + 273.15,celsius * 1.80 + 32.00};
     }
 }
