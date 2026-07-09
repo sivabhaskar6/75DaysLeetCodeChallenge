8353bae85a6438994e947baf2189420727b8cd26
GONTHINA SIVA BHASKAR <gonthinasivabhaskar6@gmail.com>
2026-07-09 10:20:37 +0530

    Time: 1 ms (13.17%), Space: 46.3 MB (91.15%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) 
+    {
+        int res[] = new int[2*n];
+        for(int i=0;i<2*n;i++)
+        {
+            if(i%2==0)
+            {
+                res[i]=nums[i/2];
+            }
+            else
+            {
+                res[i]=nums[(i+n*2)/2];
+            }
+        }
+        return res;
+    }
+}
