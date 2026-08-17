579d9cf1900ac6daddd55ba5b5091aeeefd9249f
GONTHINA SIVA BHASKAR <gonthinasivabhaskar6@gmail.com>
2026-08-17 14:14:06 +0530

    Time: 0 ms (100%), Space: 80.2 MB (78.73%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) 
+    {
+        int n= arr.length;
+        int l=0;
+        int r=n-1;
+        while(l<=r)
+        {
+            int mid =(l+r)/2;
+            if(arr[mid] < arr[mid+1])
+            {
+                l=mid+1;
+            }
+            else
+            {
+                r=mid-1;
+            }
+        }
+        return l;
+    }
+}
