938bbe4b58746c3dc34d42be04302774b6efcfc5
GONTHINA SIVA BHASKAR <gonthinasivabhaskar6@gmail.com>
2026-08-18 10:16:54 +0530

    Time: 5 ms (3.38%), Space: 268.8 MB (8%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,9 +1,7 @@
 class Solution {
-    public void reverse(int[] nums,int s, int e)
-    {
-        
-        while(s<e)
-        {
+    public void reverse(int[] nums, int s, int e) {
+
+        while (s < e) {
             int temp = nums[s];
             nums[s] = nums[e];
             nums[e] = temp;
@@ -11,17 +9,16 @@ class Solution {
             e--;
         }
     }
-    public void rotate(int[] nums, int k) 
-    {
-        int n=nums.length;
-        k=k%n;
 
-        reverse(nums,0,n-1);
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
 
-        reverse(nums,0,k-1);
+        reverse(nums, 0, n - 1);
 
-        reverse(nums,k,n-1);
+        reverse(nums, 0, k - 1);
 
+        reverse(nums, k, n - 1);
 
     }
 }
