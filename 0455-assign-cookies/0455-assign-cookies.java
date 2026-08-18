258a76e663795be626f807b852c27f93a1c9ddc9
GONTHINA SIVA BHASKAR <gonthinasivabhaskar6@gmail.com>
2026-08-18 10:03:12 +0530

    Time: 15 ms (59.59%), Space: 51.4 MB (56.35%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -7,16 +7,14 @@ class Solution {
 
         int i = 0;
         int j = 0;
-        int count = 0;
 
         while (i < g.length && j < s.length) {
             if (s[j] >= g[i]) {
-                count++;
                 i++;
             }
             j++;
         }
 
-        return count;
+        return i;
     }
 }
