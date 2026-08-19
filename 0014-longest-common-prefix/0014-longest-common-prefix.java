c61e0b6c2a7b6f7d6eebee9da408893c95b51897
GONTHINA SIVA BHASKAR <gonthinasivabhaskar6@gmail.com>
2026-08-19 15:18:31 +0530

    Time: 1 ms (61.09%), Space: 42.5 MB (99.53%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -5,14 +5,12 @@ class Solution {
         int len=0;
         String s1 = strs[0];
         String s2 = strs[strs.length-1];
-        int n = s1.length()<s2.length()?s1.length():s2.length();
-        for(int i=0;i<n;i++)
-        {
-            if(s1.charAt(i) == s2.charAt(i))
-                len++;
-            else
-                break;
+        int n = Math.min(s1.length(), s2.length());
+        int i=0;
+        while (i < n && s1.charAt(i) == s2.charAt(i)) {
+            i++;
         }
-        return len>0?s1.substring(0, len):"";
+
+        return s1.substring(0, i);
     }
 }
