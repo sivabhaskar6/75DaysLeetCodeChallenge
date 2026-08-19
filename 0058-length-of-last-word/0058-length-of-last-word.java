53bc4d8c84a7575c090e47871fcaf41d3fdd1a1a
GONTHINA SIVA BHASKAR <gonthinasivabhaskar6@gmail.com>
2026-08-19 14:47:27 +0530

    Time: 0 ms (100%), Space: 42.9 MB (69.87%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,7 +1,20 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String arr[] = s.split(" ");
-        int num = arr.length-1;
-        return arr[num].length();
+        // String arr[] = s.split(" ");
+        // int num = arr.length-1;
+        // return arr[num].length();
+        int len=0;
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            if(s.charAt(i)!=' ')
+            {
+                len++;
+            }
+            else
+            {
+                if(len>0) return len;
+            }
+        }
+        return len;
     }
 }
